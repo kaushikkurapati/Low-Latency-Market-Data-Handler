aa1ddd9be96f8fc7c825d41898bbe3a28a8e98e9
Datta Kurapati <kaushik25211@gmail.com>
2026-01-04 22:20:21 -0600

    basic encode & decode packed struct implementation

--- scripts/data_generator.py
+++ scripts/data_generator.py
@@ -1,16 +1,33 @@
 import socket
+import struct
+import time
 
 group = '224.1.1.1'
 port = 5004
 
 # 2-hop restriction in network
 ttl = 2
-sock = socket.socket(socket.AF_INET,
-                     socket.SOCK_DGRAM,
-                     socket.IPPROTO_UDP)
-sock.setsockopt(socket.IPPROTO_IP,
-                socket.IP_MULTICAST_TTL,
-                ttl)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
-for i in range(1, 10):
-    sock.sendto(b"hello world", (group, port))
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+counter = 0
+while True:
+    
+    FORMAT = '!cI4scQI'
+    message_data = b'A'
+    symbol_data = f"AAPL".encode('utf-8')
+    side_data = b'B'
+    price = 1502500
+    quantity = 10
+    
+    packed_struct = struct.pack(FORMAT, message_data, counter, symbol_data, side_data, price, quantity)
+    
+    try:
+        sock.sendto(packed_struct, (group, port))
+        print(f"Sent message {counter}")
+    except socket.error as e:
+        print(f"Socket Error {e}")
+    
+    counter += 1 
+    time.sleep(1)
--- scripts/receiver3.py
+++ scripts/receiver3.py
@@ -1,5 +1,6 @@
 import socket
 import struct
+
 MCAST_GRP = '224.1.1.1'
 MCAST_PORT = 5004
 
@@ -16,5 +17,22 @@ sock.bind(('', MCAST_PORT))
 mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
+FORMAT = '!cI4scQI'
+print(f"Listening on udp://{MCAST_GRP}:{MCAST_PORT}")
+
 while True:
-    print(sock.recv(10240))
+    data, address = sock.recvfrom(1024)
+    if data:
+        try:
+            unpacked_data = struct.unpack(FORMAT, data)
+            message_type = unpacked_data[0].decode('ascii')
+            counter_val = unpacked_data[1]
+            symbol = unpacked_data[2].decode('utf-8').strip('\0')
+            side = unpacked_data[3].decode('ascii')
+            price = unpacked_data[4]
+            quantity = unpacked_data[5]
+              
+            print(f"Received from {address}: Message='{message_type}', Counter={counter_val}, Symbol={symbol}, Side={side}, Price={price}, Quantity={quantity}")
+            
+        except struct.error as e:
+            print(f"Error unpacking data: {e}")
