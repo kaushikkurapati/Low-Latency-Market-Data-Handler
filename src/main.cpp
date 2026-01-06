b37bfd0af177e707651ea3088b487ee7575802a9
Datta Kurapati <kaushik25211@gmail.com>
2026-01-05 23:36:52 -0600

    started c++ receiver

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring> // For memset
+
+#ifdef _WIN32
+    #include <winsock2.h>
+    #pragma comment(lib, "ws2_32.lib") // Link the Winsock library automatically
+#else
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <unistd.h>
+#endif
